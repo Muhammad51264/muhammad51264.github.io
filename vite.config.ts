07dc09a71a63bb00519915389412ec0416c8527f
Muhammad51264 <muh.freijat@gmail.com>
2025-10-03 22:57:08 +0300

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import path from "path";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/my-portfolio/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {

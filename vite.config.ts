fbb89de9db65bf3e1ade262ad31f90200e2d99bd
Muhammad51264 <muh.freijat@gmail.com>
2025-10-03 22:30:00 +0300

    changes to package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import path from "path";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/my-portfolio/",
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
